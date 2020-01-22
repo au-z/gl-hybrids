6d12a0a593370d291dce03c3e768686b28ae01db
auzmartist <auz.martin@gmail.com>
2020-01-22 08:22:38 -0600

    Add helper to gl-point-light.

--- src/gl/gl-point-light.ts
+++ src/gl/gl-point-light.ts
@@ -1,11 +1,14 @@
 import * as THREE from 'three'
 import {html, Hybrids} from 'hybrids'
-
 import useGL from './useGL'
 
 interface GlPointLight extends HTMLElement {[key: string]: any}
 
-export default {
+const Helper = (value) => ({
+	get: (host) => !value ? null : new THREE.PointLightHelper(host.light, 0.5)
+})
+
+const GlPointLight = {
 	...useGL,
 	name: 'Point.001',
 	position: [0, 0, 0],
@@ -19,12 +22,18 @@ export default {
 		},
 		observe: (host, value) => console.log(host, value),
 	},
-	light: ({gl, name, position, color, intensity, distance, decay}) => {
+	light: ({gl, name, position, color, intensity, distance, decay, helper}) => {
 		const light = new THREE.PointLight(color, intensity, distance, decay)
 		light.position.set(position[0], position[1], position[2])
 		light.name = name
+		light.visible = true
 
 		gl.scene.add(light)
+
+		if(helper) {
+			gl.scene.add(helper)
+		}
+
 		return light
 	},
 	render: ({light, helper}) => html`<meta data-name="${light.name}"></meta>`
